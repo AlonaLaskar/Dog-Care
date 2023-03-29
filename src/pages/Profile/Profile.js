7c2bfa8f865f8ac85f5f1657f18a3ee4a0482e7a
Alona <alona159951@gmail.com>
2023-03-30 01:14:56 +0300

    minor fix

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -2,12 +2,10 @@ import StyledProfile from './StyledProfile';
 import AuthContext from 'providers/AuthContext';
 import { useContext } from 'react';
 
-import SocialNetworkProfile from './ProfileCard';
-
 const Profile = () => {
-  const { auth, loading } = useContext(AuthContext) || {};
+  const { userId, loading } = useContext(AuthContext) || {};
 
-  return <StyledProfile>{loading ? <div>loading...</div> : <h2> {auth?.userId}</h2>}</StyledProfile>;
+  return <StyledProfile>{loading ? <div>loading...</div> : <h2> {userId}</h2>}</StyledProfile>;
 };
 
 export default Profile;
