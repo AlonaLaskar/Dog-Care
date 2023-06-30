5b6ca942763cc0f9a482e9b733ddb12579f3cd09
Alona <alona159951@gmail.com>
2023-06-30 23:32:39 +0300

    Added missing files.

---
+++ android/app/build/intermediates/assets/release/public/static/js/3127.8b13880b.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 3127.8b13880b.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunkdog_sitter_react=self.webpackChunkdog_sitter_react||[]).push([[3127],{3127:function(t,e,r){r.r(e),r.d(e,{i:function(){return n}});var n=function(t){return t&&""!==t.dir?"rtl"===t.dir.toLowerCase():"rtl"===(null===document||void 0===document?void 0:document.dir.toLowerCase())}}}]);
+//# sourceMappingURL=3127.8b13880b.chunk.js.map
